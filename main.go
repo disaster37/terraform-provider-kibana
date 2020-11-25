795d28958d3aa191eb733930d02d88b2a8be6641
disaster37 <linuxworkgroup@hotmail.com>
2020-11-25 15:15:41 +0000

    Try to add some trace on provider

--- main.go
+++ main.go
@@ -4,9 +4,18 @@ import (
 	"github.com/disaster37/terraform-provider-kibana/v7/kb"
 
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+	log "github.com/sirupsen/logrus"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 )
 
 func main() {
+	formatter := new(prefixed.TextFormatter)
+	formatter.FullTimestamp = true
+	formatter.ForceFormatting = true
+	log.SetFormatter(formatter)
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
+
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: kb.Provider,
 	})
