af33bf86f3b526df763b234ed63c5c46ec05c4d2
disaster37 <linuxworkgroup@hotmail.com>
2021-08-24 15:21:01 +0000

    Fix test on provider
    Signed-off-by: disaster37 <linuxworkgroup@hotmail.com>

--- kb/provider_test.go
+++ kb/provider_test.go
@@ -5,34 +5,33 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-var testAccProviders map[string]terraform.ResourceProvider
+var testAccProviders map[string]*schema.Provider
 var testAccProvider *schema.Provider
 
 func init() {
 
 	// Init provider
-	testAccProvider = Provider().(*schema.Provider)
+	testAccProvider = Provider()
 	configureFunc := testAccProvider.ConfigureFunc
 	testAccProvider.ConfigureFunc = func(d *schema.ResourceData) (interface{}, error) {
 		return configureFunc(d)
 	}
-	testAccProviders = map[string]terraform.ResourceProvider{
+	testAccProviders = map[string]*schema.Provider{
 		"kibana": testAccProvider,
 	}
 
 }
 
 func TestProvider(t *testing.T) {
-	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+	if err := Provider().InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
 	}
 }
 
 func TestProvider_impl(t *testing.T) {
-	var _ terraform.ResourceProvider = Provider()
+	var _ *schema.Provider = Provider()
 }
 
 func testAccPreCheck(t *testing.T) {
